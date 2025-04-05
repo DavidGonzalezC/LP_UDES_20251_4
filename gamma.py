01f19a56a9b72ca054bfb35fd1da95b35b06761d
MEOM2001 <workmarcos12@gmail.com>
2025-04-05 12:59:53 -0500

    Marcos arregla el archivo

--- gamma.py
+++ gamma.py
@@ -1,9 +1,5 @@
 print('Hello my fish')
-<<<<<<< HEAD
-
 miVariable = 'Holaaaaa otraaaaa vezzzzz'
 print(f'Holaaaaaaaa {miVariable}')
-=======
 print("Karen Duran")
 print("David Gonzalez")
->>>>>>> a799683907b5b851d99dd8c1a0cb0fa23ee77544
